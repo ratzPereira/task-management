11124086c5f4ecda94ffc0ab578f98b213a5ea7a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-02 18:15:28 -0100

    Created the createTask service and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -8,5 +8,14 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  //we ask for the entire req body and pass in this parameter as argument
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.taskService.createTask(title, description);
   }
 }
